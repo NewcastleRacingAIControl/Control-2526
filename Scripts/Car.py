6b176d4cf3ed93ef60d2cf996ba166cdebb7cf53
Omar Jawish <c5019770@newcastle.ac.uk>
2025-11-19 16:48:18 +0000

    Add type checking.

--- Scripts/Car.py
+++ Scripts/Car.py
@@ -23,54 +23,63 @@ class Car:
         return radians
 
     def SetPosition(self, newPosition):
+        if not isinstance(newPosition, list):
+            raise KeyError("Error, position has to be a list, [x, y]")
         self.__position = newPosition
 
     def SetAcceleration(self, newAcceleration):
+        if not isinstance(newAcceleration, float):
+            raise KeyError("Error, acceleration has to be a float")
+
         if newAcceleration < -1 or newAcceleration > 1:
-            print("Error, acceleration has to be between -1 and 1")
-            return 
+            raise KeyError("Error, acceleration has to be between -1 and 1")
         self.__acceleration = newAcceleration
 
     def SetVelocity(self, newVelocity):
-        # if (newVelocity):
+        if not isinstance(newVelocity, list):
+            raise KeyError("Error, velocity should be a list, [vx, vy]")
         self.__velocity = newVelocity
 
     def SetDirection(self, newDirection):
+        if not isinstance(newDirection, float):
+            raise KeyError("Error, direction has to be a float")
         if newDirection > (2 * math.pi) or newDirection < 0:
-            print("Error, direction is in radians, must be between zero and 2 pie")
-            return
+            raise KeyError("Error, direction is in radians, must be between zero and 2 pie")
 
     def SetDirectionDegrees(self, newDirection):
+        if not isinstance(newDirection, float):
+            raise KeyError("Error, direction has to be a float")
         if newDirection > 360 or newDirection < 0:
-            print("Error, direction is in degrees, must be between 0 and 360")
-            return
+            raise KeyError("Error, direction is in degrees, must be between 0 and 360")
         self.__direction = newDirection * (math.pi / 180)
 
 
-    # def Update(self, dt):
-    #     # forward directions
-    #     fx = math.cos(self.__direction)
-    #     fy = math.sin(self.__direction)
+    def Update(self, dt):
+        # forward directions
+        fx = math.cos(self.__direction)
+        fy = math.sin(self.__direction)
 
-    #     # acceleration directions
-    #     ax = self.__acceleration * fx
-    #     ay = self.__acceleration * fy
+        # acceleration directions
+        ax = self.__acceleration * fx
+        ay = self.__acceleration * fy
 
-    #     # velocity updates
-    #     self.__velocity[0] += ax * dt
-    #     self.__velocity[1] += ay * dt
+        # velocity updates
+        self.__velocity[0] += ax * dt
+        self.__velocity[1] += ay * dt
 
-    #     # apply drag
-    #     self.__velocity[0] *= (1 - self.__drag) * dt
-    #     self.__velocity[1] *= (1 - self.__drag) * dt
+        # apply drag
+        self.__velocity[0] *= (1 - self.__drag) * dt
+        self.__velocity[1] *= (1 - self.__drag) * dt
 
-    #     # position updates
-    #     self.__position[0] += self.__velocity[0] * dt
-    #     self.__position[1] += self.__velocity[1] * dt
+        # position updates
+        self.__position[0] += self.__velocity[0] * dt
+        self.__position[1] += self.__velocity[1] * dt
 
     def PrintValues(self):
+        print("----------------------------------")
         print("Car position: " + self.__position)
         print("Car acceleration: " + self.__acceleration)
         print("Car direction: " + self.__direction)
         print("Car velocity: " + self.__velocity)
+        print("----------------------------------")
 
