7d4332f94b544f3b063d9cf559160807d61e8577
GRP-Projects <gabriel.reece.perry@outlook.com>
2025-11-19 16:10:53 +0000

    Added stepforward

---
+++ Scripts/ControlSimulation.py
@@ -0,0 +1,24 @@
+import Car
+import math
+
+class Simulation:
+
+    def __init__(self):
+        self.car = Car()
+        self.interval = 0.05
+
+    def stepForward(self):
+        pos = self.car.GetPosition()
+        dir = self.car.GetDirection()
+        acc = self.car.GetAcceleration()
+        vel = self.car.GetVelocity()
+
+        accX = math.cos(dir + math.pi/2) * acc
+        accY = math.sin(dir + math.pi/2) * acc
+
+        acc = [accX, accY]
+
+        pos = pos + (vel * self.interval) + (0.5*acc*math.pow(self.interval, 2))
+
+        self.car.SetPosition(pos)
+
